cb4b967c50f14959a5ae1f18a104948f732f9411
zaranicole3 <zns001@bucknell.edu>
2018-01-13 22:51:56 -0500

    Create validate_poi.py

---
+++ validation/validate_poi.py
@@ -0,0 +1,42 @@
+"""
+    Starter code for the validation mini-project.
+    The first step toward building your POI identifier!
+
+    Start by loading/formatting the data
+
+    After that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+#DT Classifier
+#clf = tree.DecisionTreeClassifier()
+#clf.fit(features,labels)
+#print(clf.score(features,labels))
+# overfit accuracy = 0.989473684211
+
+#split data into training and testing
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+#updated accuracy after splitting data
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+print(clf.score(features_test,labels_test))
+
+
+
